a213f7f9124c30b97ee4a23209609f777f22b0f9
Shubhan <shubhan.singh@gmail.com>
2023-04-16 17:41:21 +0530

    added

--- Week 2/src/constructors.java
+++ Week 2/src/constructors.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 class shapes{
     float radius,length,breadth;
     float Area,perimeter;
-    shapes(float r){
+    shapes(float r){//Overloaded constructor for circle
         radius=r;
         Area=Area(radius);
         perimeter=perimeter(radius);
     }
-    shapes(float l, float b){
+    shapes(float l, float b){//Overloaded constructor for rectangle
         length=l;
         breadth=b;
         Area=Area(length,breadth);
@@ -16,20 +16,20 @@ class shapes{
     }
     float Area(float r){
         return (float) (3.14*r*r);
-    }
+    }//Area of circle
     float Area(float l, float b){
         return l*b;
-    }
+    }//area of rectangle
     float perimeter(float r){
         return (float) (2*3.14*r);
-    }
+    }//circ. of circle
     float perimeter(float l, float b){
         return 2*(l+b);
-    }
+    }//perimeter of rectangle
 
 }
 public class constructors {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//Driver code
         Scanner sc= new Scanner(System.in);
         float r,l,b;
         System.out.println("Enter radius of circle and length and breadth of rectangle");
