746b90d6ce5edf791b92c31a271d41af13ebd8f5
Shubhan <shubhan.singh@gmail.com>
2023-04-22 22:38:21 +0530

    added

--- Week 3/src/Sc_calc.java
+++ Week 3/src/Sc_calc.java
@@ -1,27 +1,49 @@
 import java.lang.Math;
-    class Test{
-        double base,ans;
-        int power;
-        int log_base,argument;
-        Test(){
-            base=power=log_base=argument=2;
-        }
-        void Calculate(double base, int power){
-            ans=Math.pow(base,power);
-        }
-        void Calculate(int log_base, int argument){
-            ans=Math.log(argument)/Math.log(log_base);
-        }
-        void display(){
-            System.out.println(ans);
-        }
+
+// Define a class named Test
+class Test {
+    // Declare instance variables
+    double base, ans;
+    int power;
+    int log_base, argument;
+
+    // Define a constructor that sets all instance variables to 2
+    Test() {
+        base = power = log_base = argument = 2;
     }
+
+    // Define a method to calculate base raised to the power of power
+    void Calculate(double base, int power) {
+        ans = Math.pow(base, power);
+    }
+
+    // Define a method to calculate the logarithm of argument to the base of log_base
+    void Calculate(int log_base, int argument) {
+        ans = Math.log(argument) / Math.log(log_base);
+    }
+
+    // Define a method to display the value of ans
+    void display() {
+        System.out.println(ans);
+    }
+}
+
+// Define a public class named Sc_calc
 public class Sc_calc {
     public static void main(String[] args) {
-        Test obj=new Test();
-        obj.Calculate(2,4);
+        // Create an instance of the Test class
+        Test obj = new Test();
+
+        // Call the Calculate method with a double and an integer argument
+        obj.Calculate(2, 4);
+        System.out.print("The value of log 4 base 2 is: ");
+        // Display the result
         obj.display();
-        obj.Calculate(2.0,4);
+
+        // Call the Calculate method with a double and an integer argument
+        obj.Calculate(2.0, 4);
+        System.out.print("The value of 2^4 is: ");
+        // Display the result
         obj.display();
     }
 }
